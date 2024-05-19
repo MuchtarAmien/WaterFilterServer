5b5eb0b6f1ff99d0a6ad29d2374aa0e6bbe7ef46
MuchtarAmien <muchtaramien3@gmail.com>
2024-05-19 15:48:53 +0700

    init

--- api_device/v1/controller.js
+++ api_device/v1/controller.js
@@ -1,6 +1,7 @@
 const { resError, resSuccess } = require("../../services/responseHandler")
 const { generateString } = require("../../services/stringGenerator")
 const prisma = require("../../prisma/client")
+const Record = require('../models/Record');
 
 exports.deviceList = async (req, res) => {
     try {
@@ -29,16 +30,33 @@ exports.generateDeviceId = async (req, res) => {
         return resError({ res, errors: error })
     }
 }
+// 1. Ambil Payload Yang Di Kirimkan Oleh ESP nantinya melalui request body
+// Payload yang diperlukan : kode_unik_perangkat, moniotr_kekurahan, monitor_ph, monitor_tds
+// 2. Update table perangkat dengan nilai yang baru di terima
+// 3. Tambahkan record tersebut ke dalam tabel "Riwayat" jika selisih waktu antara record perangkat di database di banding dengan masuknya
+// request baru berselang 1 menit, tetapi jika record perangkat kosong lalu masukan saja record baru
 
 exports.generateRecord = async (req, res) => {
     try {
-        // 1. Ambil Payload Yang Di Kirimkan Oleh ESP nantinya melalui request body
-        // Payload yang diperlukan : kode_unik_perangkat, moniotr_kekurahan, monitor_ph, monitor_tds
-        // 2. Update table perangkat dengan nilai yang baru di terima
-        // 3. Tambahkan record tersebut ke dalam tabel "Riwayat" jika selisih waktu antara record perangkat di database di banding dengan masuknya
-        // request baru berselang 1 menit, tetapi jika record perangkat kosong lalu masukan saja record baru
-        return resSuccess({ res, title: "Success to create new record" })
+        // Mendapatkan data dari body permintaan
+        const { kode_unik, control_motor_dc, monitor_kekeruhan, monitor_ph, monitor_tds } = req.body;
+
+        // Membuat catatan baru
+        const newRecord = new Record({
+            kode_unik,
+            control_motor_dc,
+            monitor_kekeruhan,
+            monitor_ph,
+            monitor_tds
+        });
+
+        // Menyimpan catatan baru ke dalam database
+        await newRecord.save();
+
+        // Mengirim respons ke klien bahwa catatan baru telah berhasil dibuat
+        return res.status(200).json({ success: true, message: "Success to create new record", data: newRecord });
     } catch (error) {
-        return resError({ res, errors: error })
+        // Mengirim respons ke klien jika terjadi kesalahan
+        return res.status(500).json({ success: false, message: "Failed to create new record", error: error.message });
     }
-}
+};
