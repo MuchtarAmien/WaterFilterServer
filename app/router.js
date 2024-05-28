183efa0d4c7a5e3a4edbb6dc467f61256cddaafe
MuchtarAmien <muchtaramien3@gmail.com>
2024-05-28 12:46:57 +0700

    init

--- app/router.js
+++ app/router.js
@@ -1,6 +1,22 @@
-const router = require("express").Router();
-const controller = require("./controlers");
+// router.js
 
-router.get("/", controller.tes);
+// Import module express
+const express = require('express');
 
+// Inisialisasi router
+const router = express.Router();
+
+// Import controller
+const controller = require('./controller');
+
+// Definisikan fungsi callback untuk route GET
+function getHandler(req, res) {
+    // Mengirimkan respon ke client
+    res.send('Ini adalah contoh respon dari route GET');
+}
+
+// Menambahkan route GET dengan fungsi callback yang valid
+router.get("/", getHandler);
+
+// Export router agar dapat digunakan di file lain
 module.exports = router;
