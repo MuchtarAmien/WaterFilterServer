0eb484612d0c0c352eb296944781e4126d8131b4
MuchtarAmien <muchtaramien3@gmail.com>
2024-07-29 21:13:51 +0700

    fungsi generate10

--- api_device/v1/controller.js
+++ api_device/v1/controller.js
@@ -3,17 +3,6 @@ const { generateString } = require("../../services/stringGenerator");
 const { sendTelegramMessageByKodeUnik } = require("../../services/telegramServices"); // Impor layanan Telegram
 const prisma = require("../../prisma/client");
 
-exports.deviceList = async (req, res) => {
-    try {
-        return resSuccess({
-            res,
-            title: "Success to show all device list",
-            data: ["device 1", "device 2"],
-        });
-    } catch (error) {
-        return resError({ res, errors: error });
-    }
-};
 
 exports.generateDeviceId = async (req, res) => {
     console.log("generateDeviceId endpoint hit1");
@@ -34,6 +23,18 @@ exports.generateDeviceId = async (req, res) => {
             res,
             title: "Success to create new device",
             data: { device_id: kodeUnikPerangkat },
+        });
+    } catch (error) {
+        return resError({ res, errors: error });
+    }
+};
+
+exports.deviceList = async (req, res) => {
+    try {
+        return resSuccess({
+            res,
+            title: "Success to show all device list",
+            data: ["device 1", "device 2"],
         });
     } catch (error) {
         return resError({ res, errors: error });
